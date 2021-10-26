08215e6afa5951df9a4fa7fa27eb3307e6f79d5d
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-10-26 17:41:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let js ="amazing";
-if (js === "amazing") alert("JavaScript is FUN");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+
+let firstName = "Jonas"
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
