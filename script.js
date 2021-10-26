574aabcb1597ee46bc66cdc54a90ab83158c9dcc
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2021-10-26 18:26:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ let firstName = "Jonas"
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+/*LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+let country = "United States"
+let continent = "North America"
+let population = "1.3 Million"
+console.log(country , continent , population);
+console.log(country);
+console.log(continent);
+console.log(population);
